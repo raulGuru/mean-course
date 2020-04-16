be0d00006c74d20a3bcc6bde967ff9a7854d56af
rahulg <karizmar_225@yahoo.com>
2020-04-16 23:11:43 +0530

    routing, styling links, updating post, spinner

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -28,41 +28,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  // console.log(post);
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "post added to MongoDB",
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "posts fetched to MongoDB",
-      posts: documents,
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    // console.log(result);
-    res.status(200).json({
-      message: req.params.id + "postId deleted from MongDB",
-    });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Post } from './post.model';
 
@@ -10,7 +11,7 @@ export class PostService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
@@ -36,16 +37,40 @@ export class PostService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
       .subscribe((resData) => {
         //console.log(resData);
         const id = resData.postId;
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
+      });
+  }
+
+  udpatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const udpatedPosts = [...this.posts];
+        const oldPostIndex = udpatedPosts.findIndex((p) => p.id === post.id);
+        udpatedPosts[oldPostIndex] = post;
+        this.posts = udpatedPosts;
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
   }
 
